dd846b83089be017b7d98897759f3d6a1c4bd5a8
Glenunciter <pourcinematteo@gmail.com>
2022-06-11 10:42:12 +0200

    final final

--- sauvegarde.c
+++ sauvegarde.c
@@ -1,5 +1,15 @@
 #include <sauvegarde.h>
 
+
+/***********************************************************************************************************************/
+/**
+ * @brief Sert à  sauvegarder une partie au cours du jeu 
+ * 
+ * @param filepath --> Nom du ficher qui va être créé ou ouvert 
+ * @param taille --> taille de l'echiquier
+ * @param tour --> Le nombre de tour de la partie actuelle 
+ * @param echiquier --> structure de pièce 
+ */
 void Sauvegarde(const char* filepath, int taille, int tour, Piece** echiquier)
 {
     FILE* fichier = fopen(filepath, "w");
@@ -13,8 +23,15 @@ void Sauvegarde(const char* filepath, int taille, int tour, Piece** echiquier)
     }
     fclose(fichier);
 }
-
-
+/***********************************************************************************************************************/
+/**
+ * @brief Sert à lire un fichier .txt pour reprendre une partie sauvegarder 
+ * 
+ * @param filepath --> Nom du ficher 
+ * @param taille  --> taille de l'echiquier
+ * @param tour -->  Le nombre de tour de la partie actuelle
+ * @param echiquier  --> structure de pièce 
+ */
 Piece** LectureSauvegarde(const char* filepath,int* taille,int* tour){
     
     Piece** echiquier;
@@ -33,8 +50,14 @@ Piece** LectureSauvegarde(const char* filepath,int* taille,int* tour){
     }
     return echiquier;
 }
-
-
+/***********************************************************************************************************************/
+/**
+ * @brief Sert à initializer un echiquier à partir d'une sauvegarde (si fichier corrumpu le programme s'ârrete il sera utile de supprimer le .txt si vous voulez jouer)
+ * 
+ * @param fichier --> addresse du fichier 
+ * @param taille --> taille de l'echiquier
+ * @return Piece** --> echiquier initializer si bien allouer 
+ */
 Piece** InitializeEchiquierS(FILE* fichier,int taille)
 {
     
@@ -71,3 +94,4 @@ Piece** InitializeEchiquierS(FILE* fichier,int taille)
     
     return echiquier;
 }    
+/***********************************************************************************************************************/
--- sauvegarde.h
+++ sauvegarde.h
@@ -6,28 +6,29 @@
 #include <echiquier.h>
 
 /**
- * @brief 
+ * @brief Sert à  sauvegarder une partie au cours du jeu 
  * 
- * @param filepath 
- * @param taille 
- * @param tour 
- * @param echiquier 
+ * @param filepath --> Nom du ficher qui va être créé ou ouvert 
+ * @param taille --> taille de l'echiquier
+ * @param tour --> Le nombre de tour de la partie actuelle 
+ * @param echiquier --> structure de pièce 
  */
 void Sauvegarde(const char* filepath, int taille, int tour, Piece** echiquier);
 /**
- * @brief 
+ * @brief Sert à lire un fichier .txt pour reprendre une partie sauvegarder 
  * 
- * @param filepath 
- * @param taille 
- * @param tour 
- * @param echiquier 
+ * @param filepath --> Nom du ficher 
+ * @param taille  --> taille de l'echiquier
+ * @param tour -->  Le nombre de tour de la partie actuelle
+ * @param echiquier  --> structure de pièce 
  */
 Piece** LectureSauvegarde(const char* filepath,int* taille,int* tour);
 /**
- * @brief 
+ * @brief Sert à initializer un echiquier à partir d'une sauvegarde (si fichier corrumpu le programme s'ârrete il sera utile de supprimer le .txt si vous voulez jouer)
  * 
- * @param taille 
- * @return Piece** 
+ * @param fichier --> addresse du fichier 
+ * @param taille --> taille de l'echiquier
+ * @return Piece** --> echiquier initializer si bien allouer 
  */
 Piece** InitializeEchiquierS(FILE* fichier,int taille);
 
